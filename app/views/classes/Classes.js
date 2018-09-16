58fc3cbfe44cf0a71e90365a409ee1920b844f7f
AdrianoASJ <adrianosousa.junior@gmail.com>
2018-09-16 18:46:53 -0300

    adicionando refresh na lista de classes e trazendo a lista correta

--- app/views/classes/Classes.js
+++ app/views/classes/Classes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View, Image, FlatList, TouchableWithoutFeedback } from 'react-native';
+import { StyleSheet, View, ScrollView, FlatList, TouchableWithoutFeedback, RefreshControl } from 'react-native';
 import { Header, Card, Text, Icon, Button } from 'react-native-elements'
 import { Constants } from '../../Constants'
 import * as firebase from 'firebase';
@@ -13,11 +13,9 @@ export class Classes extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            classes: []
+            classes: [],
+            refreshing: false
         }
-    }
-
-    componentDidMount = () => {
         this.loadClasses()
     }
 
@@ -36,18 +34,25 @@ export class Classes extends React.Component {
 
     loadClasses = () => {
         const { currentUser } = firebase.auth();
+        console.log('CURRENT')
+        console.log(currentUser.uid)
 
         ref = firebase.firestore().collection("classes")
         let array = []
-        ref.where("name", "==", "Apis1").get().then(function(querySnapshot) {
+        ref.where("professorUid", "==", currentUser.uid).get().then(function(querySnapshot) {
             querySnapshot.forEach(function(doc) {
                 array.push(doc.data());
             })
-            this.setState({ classes: array})
+            this.setState({ classes: array, refreshing: false})
         }.bind(this)).catch(function (error) {
             console.log(error)
             alert(error.message)
         })
+    }
+
+    onRefresh = () => {
+        this.setState({ refreshing: true})
+        this.loadClasses()
     }
     
     render() { 
@@ -69,40 +74,49 @@ export class Classes extends React.Component {
                         />}
                     />
 
-                <Button
-                    title="NOVA TURMA" 
-                    titleStyle={{ fontWeight: '700'}}
-                    buttonStyle={{marginTop: 20, backgroundColor: Constants.Colors.Primary}}
-                    onPress={() => this.props.navigation.navigate('NewClass')}
-                />
+                <ScrollView
+                    refreshControl={
+                        <RefreshControl refreshing={this.state.refreshing} onRefresh={this.onRefresh}/>
+                    }
+                >
 
-                <FlatList
-                data={this.state.classes}
-                keyExtractor={item => item.uid.toString()}
-                renderItem={({item}) => (
-                    <TouchableWithoutFeedback
-                    onPress={() => this.props.navigation.navigate('MaterialTabs')}
-                    >
-                    <Card
-                        flexDirection="row"
-                    >
-                        <Icon
-                            raised
-                            containerStyle={{backgroundColor:'#AFAFAF'}}
-                            name='class'
-                            color='#f1f1f1'
-                            />
-                         <View style={{marginLeft: 20}}>
-                            <Text
-                            style={{fontFamily: 'montserrat_thin'}}
-                            h4>{item.name}</Text>
-                            <Text>{item.professor}</Text>
-                            <Text style={{color: "gray"}}>{item.alunos} alunos</Text>
-                        </View>
-                    </Card>
-                    </TouchableWithoutFeedback>
-                )}
-                />
+                    <Button
+                        title="NOVA TURMA" 
+                        titleStyle={{ fontWeight: '700'}}
+                        buttonStyle={{marginTop: 20, backgroundColor: Constants.Colors.Primary}}
+                        onPress={() => this.props.navigation.navigate('NewClass')}
+                    />
+
+                    <FlatList
+                    data={this.state.classes}
+                    keyExtractor={item => item.uid.toString()}
+                    renderItem={({item}) => (
+                        <TouchableWithoutFeedback
+                        onPress={() => this.props.navigation.navigate('MaterialTabs')}
+                        >
+                        <Card
+                            flexDirection="row"
+                        >
+                            <Icon
+                                raised
+                                containerStyle={{backgroundColor:'#AFAFAF'}}
+                                name='class'
+                                color='#f1f1f1'
+                                />
+                            <View style={{marginLeft: 20, width: 0, flexGrow: 1, flex: 1}}>
+                                <Text
+                                fontFamily='montserrat_semi_bold'
+                                style={{color: Constants.Colors.Primary}}
+                                h5>{item.name}</Text>
+                                {/* <Text>{item.professor}</Text> */}
+                                {/* <Text style={{color: "gray"}}>{item.alunos} alunos</Text> */}
+                            </View>
+                        </Card>
+                        </TouchableWithoutFeedback>
+                    )}
+                    />
+
+                </ScrollView>
 
             </View>
         );
